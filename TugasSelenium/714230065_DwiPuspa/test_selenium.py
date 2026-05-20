12f78b150f6f88a91ba83168049ca803d8ea1150
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2026-05-20 23:15:05 +0700

    update selenium

--- TugasSelenium/714230065_DwiPuspa/test_selenium.py
+++ TugasSelenium/714230065_DwiPuspa/test_selenium.py
@@ -3,14 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-# Menyiapkan dan membuka browser Chrome
 browser = webdriver.Chrome()
+
+#Maksimalkan ukuran jendela browser agar kolom pencarian tidak tersembunyi!
+browser.maximize_window() 
 
 # Mengakses halaman utama Wikipedia Bahasa Indonesia
 browser.get("https://id.wikipedia.org/")
 
-#  Menemukan kolom pencarian Wikipedia (menggunakan atribut NAME "search")
-kolom_pencarian = browser.find_element(By.NAME, "search")
+# menggunakan CSS_SELECTOR agar Selenium langsung menemukan kolom yang terlihat
+kolom_pencarian = browser.find_element(By.CSS_SELECTOR, "input[type='search']")
 
 kata_kunci = "Pengujian perangkat lunak"
 kolom_pencarian.send_keys(kata_kunci)
@@ -20,9 +22,10 @@ kolom_pencarian.send_keys(Keys.RETURN)
 
 time.sleep(4)
 
-# Mencari elemen judul artikel utama (menggunakan ID "firstHeading") dan mengambil teksnya
+# Mencari elemen judul artikel utama (menggunakan ID "firstHeading")
 judul_artikel = browser.find_element(By.ID, "firstHeading").text
 
 print(f"✨ Pengujian Sukses! Artikel yang berhasil dibuka berjudul: '{judul_artikel}'")
 
+time.sleep(2)
 browser.quit()
